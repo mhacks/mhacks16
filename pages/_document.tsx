04f5a7258b4d1f9a40674ca2b53761f67671568e
Cooper Saye <coopersaye@gmail.com>
2023-08-20 13:44:28 -0700

    Set up page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@800&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,36 @@
 import styles from '@/styles/pages/Index.module.scss'
+import Image from 'next/image'
 
 export default function Index() {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <div className={styles.titleImage}>
+          <Image src='/img/title.svg' alt='title.svg' layout='fill' />
+        </div>
+
+        <p>
+          Michigan’s premier hackathon is{' '}
+          <span className={styles.alt}>
+            back in person. November 18-19, 2023.
+          </span>{' '}
+          Applications open soon.
+        </p>
+
+        <p>
+          If you’re interested in becoming a sponsor, please reach out at{' '}
+          <a href='mailto:sponsorship@mhacks.org'>sponsorship@mhacks.org.</a>
+        </p>
+      </div>
+
+      <a
+        className={styles.footerText}
+        href='https://v1michigan.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        presented-by-v1@michigan ~ % ▓
+      </a>
+    </div>
+  )
 }
