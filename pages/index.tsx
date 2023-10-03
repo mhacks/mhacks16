fdbb81b34d0bc86da5732f45bb53fa1c8d2c7e81
Cooper Saye <coopersaye@gmail.com>
2023-10-02 21:38:44 -0400

    Create video section

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,6 +61,32 @@ export default function Index() {
           </p>
         </div>
       </div>
+
+      <div className={styles.stats}>
+        <video src='/mhacks4.mov' autoPlay loop muted />
+
+        <h2>The Most Hype Hackathon</h2>
+
+        <p>
+          Over the last 10 years, MHacks defined what a great hackathon could
+          be. MHacks 16 is a reboot of the world’s most legendary hackathon.
+        </p>
+
+        <div className={styles.numbers}>
+          <div>
+            <h3>13,000+</h3>
+            <p>Attendees</p>
+          </div>
+          <div>
+            <h3>15</h3>
+            <p>Hackathons</p>
+          </div>
+          <div>
+            <h3>$350,000+</h3>
+            <p>in Prizes</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
