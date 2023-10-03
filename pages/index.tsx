e1825bd8a0beb7cc018001d95d2fdb14c8113de4
Cooper Saye <coopersaye@gmail.com>
2023-10-02 21:22:52 -0400

    Create what's a hackathon section

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,6 +37,30 @@ export default function Index() {
           </div>
         </div>
       </div>
+
+      <div className={styles.hackathon}>
+        <Image
+          src='/img/speaking.png'
+          alt='speaking.png'
+          width='500'
+          height='300'
+        />
+
+        <div className={styles.what}>
+          <h2>What’s a Hackathon?</h2>
+          <br />
+          <p>
+            Hackathons are a place for like-minded students to create amazing
+            projects. Whether you’re a veteran builder or you’ve never been to a
+            hackathon before, MHacks has something for you.
+          </p>
+          <br />
+          <p>
+            Come to MHacks to <b>learn</b>, come to <b>build</b>, and come to
+            create <b>life-long friendships</b>.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
