0f81d82234914bf8681718c3c27a5c15fed5ad95
Brian Travis <bztravis88@gmail.com>
2023-10-13 22:24:20 -0400

    Add special thanks sponsors

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -14,13 +14,19 @@ const sponsorRows = [
     companies: [
       { logo: 'ramp', url: 'http://www.ramp.com' },
       { logo: 'qualcomm', url: 'http://www.qualcomm.com' },
-      { logo: 'ford', url: 'http://www.ford.com' },
+      // { logo: 'ford', url: 'http://www.ford.com' },
       { logo: 'viralitydata', url: 'http://www.viralitydata.com' },
       { logo: 'roboflow', url: 'http://www.roboflow.com' },
       { logo: 'optimize', url: 'http://www.optimizemi.org' },
       { logo: 'engineering', url: 'http://www.engin.umich.edu' },
     ]
   }
+];
+const specialCompanies = [
+  { logo: 'uber', url: 'https://www.uber.com' },
+  { logo: 'undergroundprinting', url: 'https://www.undergroundprinting.com' },
+  { logo: 'googlecloud', url: 'https://www.cloud.google.com' },
+  { logo: 'rossbusinesstech', url: 'https://michiganross.umich.edu/about/business-tech' },
 ];
 
 export default function Sponsors() {
@@ -41,6 +47,13 @@ export default function Sponsors() {
           <div className={styles.sponsorRow}>
             {elem.companies.map(company => (<a href={company.url} target="_blank"><img className={styles.sponsorLogo} src={`/img/sponsors/${company.logo}.png`} alt={company.logo} /></a>))}
           </div>))}
+        <h2>Special thanks to:</h2>
+        <div className={styles.sponsorRow}>
+
+          {specialCompanies.map((company) => (
+            <a href={company.url} target="_blank"><img className={styles.sponsorLogo} src={`/img/sponsors/${company.logo}.png`} alt={company.logo} /></a>)
+          )}
+        </div>
       </div>
       <a
         className={styles.becomeContainer}
