083e41e31ad775269551519c15aa2e1a59c74296
RaghavBhat02 <bhatr@umich.edu>
2023-11-18 21:10:47 -0500

    🥅 better error handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,9 @@ const image = 'https://mhacks.org/img/background.png'
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
-
+  console.error(
+    'Warning: Encountered two children with the same key, `Daniel Malis`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version. \n    at div    \n    at div \n    at MentorThanks \n    at div \n    at Index \n    at App (https://nsadnkwjqdhwqoudho23uo89u20odij2odi23jdpo28dijo2iduo9iu.mhacks.org)\n    at PathnameContextProviderAdapter (webpack-internal:///./node_modules/next/dist/shared/lib/router/adapters.js:80:11) \n    at ErrorBoundary (webpack-internal:///./node_modules/next/dist/compiled/@next/react-dev-overlay/dist/client.js:306:63) \n    at ReactDevOverlay (webpack-internal:///./node_modules/next/dist/compiled/@next/react-dev-overlay/dist/client.js:858:919)\n    at Container (webpack-internal:///./node_modules/next/dist/client/index.js:78:1)\n    at AppContainer (webpack-internal:///./node_modules/next/dist/client/index.js:182:11)\n    at Root (webpack-internal:///./node_modules/next/dist/client/index.js:372:11)'
+  )
   return (
     <>
       <Head>
@@ -29,7 +31,6 @@ export default function App(props: AppProps) {
         <meta name='twitter:title' content={title} />
         <meta name='twitter:description' content={description} />
         <meta name='twitter:image' content={image} />
-
         <link rel='icon' href='/icons/favicon.ico' />
         <link
           rel='apple-touch-icon'
