0ac112ddbb1362b4365e6178fbd21d13aa42a766
Brian Travis <bztravis88@gmail.com>
2024-02-07 23:34:02 -0500

    Update nuked landing page for deployment

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,31 +1,15 @@
-import Image from 'next/image';
-import styles from '../styles/components/Landing.module.scss';
-
-// In your component's render method or functional component body
-document.body.classList.add(styles.noScroll);
+import Image from 'next/image'
+import styles from '../styles/components/Landing.module.scss'
 
 export default function Landing() {
   return (
     <div className={styles.landing}>
       <div className={styles.terminal}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src='/img/logo_m.svg' alt='logo.svg' className={styles.logo}/>
+        <img src='/img/logo_m.svg' alt='logo.svg' className={styles.logo} />
 
-        <div className={styles.hype}>
-          Coming soon.
-        </div>
-
-        {/* <div className={styles.apply}>
-          <a
-            href='https://tally.so/r/mKx51K'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Apply Now
-          </a>
-          <p>Due Oct 30</p>
-        </div> */}
+        <div className={styles.hype}>Coming soon...</div>
       </div>
     </div>
-  );
+  )
 }
