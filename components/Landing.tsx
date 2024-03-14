dd58ab089d87eef93644c2cff72052b784af8f21
Brian Travis <bztravis88@gmail.com>
2024-03-14 00:31:04 -0400

    Add redirect to application form

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import styles from '../styles/components/Landing.module.scss'
 
+// Remove This For Real MHacks
+import { useEffect } from 'react'
+
 export default function Landing() {
-  return (
-    <div className={styles.landing}>
-      <div className={styles.terminal}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
+  useEffect(() => {
+    window.location.href = 'https://tally.so/r/w2eB7j'
+  }, [])
+
+  return <div className={styles.terminal}>Redirecting...</div>
+}
+
+/**
+<div className={styles.terminal}>
         <img src='/img/logo_m.svg' alt='logo.svg' className={styles.logo} />
 
         <h3>Interested in sponsoring, or just have a question?</h3>
@@ -13,6 +21,5 @@ export default function Landing() {
         <p>Having trouble with the link?</p>
         <p>Paste our email in: <a href="mailto:hackathon@umich.edu">hackathon@umich.edu</a></p>
       </div>
-    </div>
-  )
-}
+      
+*/
