e8f7665bb1d6d4ed6edc7e8f70960390ec8edad5
Brian Travis <bztravis@umich.edu>
2024-03-14 09:50:39 -0400

    Refactor redirect to application form to use next config redirects

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image'
 import styles from '../styles/components/Landing.module.scss'
 
-// Remove This For Real MHacks
-import { useEffect } from 'react'
+// redirect defined in next.config.js
 
 export default function Landing() {
-  useEffect(() => {
-    window.location.href = 'https://tally.so/r/w2eB7j'
-  }, [])
-
-  return <div className={styles.terminal}>Redirecting...</div>
-}
-
-/**
-<div className={styles.terminal}>
+  return (
+    <div className={styles.landing}>
+      <div className={styles.terminal}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src='/img/logo_m.svg' alt='logo.svg' className={styles.logo} />
 
         <h3>Interested in sponsoring, or just have a question?</h3>
-        <a href="mailto:hackathon@umich.edu" className={styles.hype}>Contact Us</a>
+        <a href='mailto:hackathon@umich.edu' className={styles.hype}>
+          Contact Us
+        </a>
         <p>Having trouble with the link?</p>
-        <p>Paste our email in: <a href="mailto:hackathon@umich.edu">hackathon@umich.edu</a></p>
+        <p>
+          Paste our email in:{' '}
+          <a href='mailto:hackathon@umich.edu'>hackathon@umich.edu</a>
+        </p>
       </div>
-      
-*/
+    </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: 'https://tally.so/r/w2eB7j',
+        permanent: true,
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
